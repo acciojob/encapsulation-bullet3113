7af8af92cc34f7b945fc2d82788758be897d1d8e
bullet3113 <0hitesh.a@gmail.com>
2023-10-10 08:58:36 +0530

    assignment complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    
+    RWOnly obj = new RWOnly();
+
+    // obj.data = 10;
+    //  The field RWOnly.data is not visible
+
+    // setting value 10 
+    obj.setter(10);
+
+    // getting the data back
+    System.out.println(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int data;
+
+    public int getter() {
+        return this.data;
+    }
+
+    public void setter(int data) {
+        this.data = data;
+    }
 }
